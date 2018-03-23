31e0dc48dc02ca03e702b6de58d52a74c50a554c
Mosaddeq <36484786+Mosaddeq@users.noreply.github.com>
2018-03-24 03:32:00 +0600

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -10,6 +10,6 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
     die("Connection Error!".mysqli_connect_error());
 }
-    $sql = "insert into reg values ('".$name."','".$email."','".$gender."','".$pass."','".$uType."')";
+    
 
 ?>
