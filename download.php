97ab0c47ae8d8c96be601d8babe2f5393a230563
Mosaddeq <36484786+Mosaddeq@users.noreply.github.com>
2018-03-25 04:46:21 +0600

    Add files via upload

---
+++ download.php
@@ -0,0 +1,24 @@
+<?php
+require("dbconnect.php");
+$sql="select * from upload";
+$query=mysqli_query($conn,$sql);
+$rowcount = mysqli_num_rows($query); //count rows
+?>
+<table border="1">
+<tr>
+<td>Files</td>
+</tr>
+<?php
+for($i=1;$i<=$rowcount;$i++) //to fetch all data 
+{
+  $row = mysqli_fetch_array($query); //fetch rows
+}
+?>
+<tr>
+<td><a href="upload/"><?php echo $row["file"] ?>"><?php echo $row["file"] ?></td>
+</tr>
+<a href="studenthome.php">Back</a>
+
+
+
+
