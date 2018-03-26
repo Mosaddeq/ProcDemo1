d9f94056ed6fc108a7f53e5de05e0b3a2885c173
Mosaddeq <36484786+Mosaddeq@users.noreply.github.com>
2018-03-27 02:15:12 +0600

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,62 +1,75 @@
-<?php
-$nameErr = $emailErr = $genderErr = $passErr = $confirmErr = $checkErr = $typeErr = ""; 
-
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-	$gender = $_POST['gender'];
-    $pass =  $_POST['pass'];
-    $confirm =  $_POST['confirm'];
-    $uType =  $_POST['uType'];
-    
-    include ("funct.php");
-    require ("dbconnect.php");
-    require ("validation.php");
-
-    $sql = "insert into reg values ('".$name."','".$email."','".$gender."','".$pass."','".$uType."')";
-}
-?>
+<script >
+	<?php
+	$nameErr = $emailErr = $genderErr = $passErr = $confirmErr = $checkErr = $typeErr = ""; 
+	 //$gender="";
+	
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$gender = $_POST['gender'];
+		$pass =  $_POST['pass'];
+		$confirm =  $_POST['confirm'];
+		$uType =  $_POST['uType'];
+		
+		include ("funct.php");
+		require ("dbconnect.php");
+		require ("validation.php");
+	}
+	?>
+	</script>
 <!DOCTYPE HTML>
 
 <html>
-<title>Registration</title>
+<title>A Comp</title>
 <head>
-<h1 align="center">Registration Form</h1>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
-<body>
-<div>
+<body> 
+<div class="col-lg-8 m-auto d-block">
+<h1 class="text-success text-center" >Complete Registration </h1>
 
-<form method="POST" action="#">   <!$_SERVER["PHP_SELF"]Returns the filename of the currently executing script> 
-    Name: <br>
-	<input type="text" name="name" value="" placeholder="Full Name"/>
-	<span class="error">* <?php echo $nameErr;?></span>								<! display errors on html page>
+<form method="POST" action="#" onsubmit="return validation()" class="bg-light" >   <!$_SERVER["PHP_SELF"]Returns the filename of the currently executing script> 
+    <label>Name:</label> <br>
+	<input  id="name" type="text" name="name" value="" class="form-control" placeholder="Full Name"/>
+	<span class="error">* <?php echo $nameErr;?></span>	
+	<span id="name"	class="text-danger font-witght-bold"></span>							<! display errors on html page>
 	<br>
 	<br>
-	Email: <br>
-		<input type="text" name="email" value="" placeholder="********@****.com" />
-		<span class="error">* <?php echo $emailErr;?></span>	
+	<label>Email:</label> <br>
+		<input id="email" type="text" name="email" class="form-control" value="" placeholder="********@****.com" />
+		<span class="error"  >* <?php echo $emailErr;?></span>	
 		<br>
 		<br>
-	Gender: <br>
-		<input type="radio" name="gender" value="Female">Female
-		<input type="radio" name="gender" value="Male">Male
+		<label>Gender: </label><br>
+		<input id="gender" type="radio"  name="gender" value="Female">Female
+		<input id="gender" type="radio"  name="gender" value="Male">Male
 		<span class="error">* <?php echo $genderErr;?></span>	
 		<br><br>
-	Password:<br>
-	<input type="password" name="pass" value="" placeholder="*********"/>
+		<label>Password:</label><br>
+	<input id="pass" type="password" class="form-control" name="pass" value="" placeholder="*********"/>
 		<span class="error">* <?php echo $passErr;?></span>	
 		<br>
 		<br>
-	Confirm Password:<br>
-		<input type="password" name="confirm" value="" placeholder="*********"/>
-		<span class="error">* <?php echo $confirmErr;?></span>	
+		<label>Confirm Password:</label><br>
+		<input id="confirm"  type="password" class="form-control" name="confirm" value="" placeholder="*********"/>
+		<span class="error">* <?php echo $confirmErr;?></span>
+		
 		<br>
 		<br>
-	User Type:
-		<select value="combo" name="uType" >
+		<label>User Type:</label>
+		<select id="uType" value="combo" class="form-control" name="uType" >
 		<option value=""></option>
 		<option value="Instructor">Instructor</option>
 		<option value="Student">Student</option>
@@ -64,11 +77,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 		</select>
 		<br>
 		<br>
-        <input type="checkbox" name="check" value="checked" placeholder="" />Yes,I've read the <a href="terms and conditions.html">terms and conditions </a> and i agree
+        <input id="check"  type="checkbox" name="check" value="checked" placeholder="" />Yes,I've read the <a href="terms and conditions.html">terms and conditions </a> and i agree
 		<span class="error">* <?php echo $checkErr;?></span>	
 		<br>
 		<br>
-		<input type="submit" name="submit" value="submit"/>
+		<input type="submit" name="submit" value="Submit" class ="btn btn-success" /> 
 
-        <?php include ("foot.php"); ?>
+		<a href="login.php">Cancel</a> <br>
+
+    	
+		</div>
+		<script type="text/javascript">
+		function validation() {
+			var error_is = 0;
+			var name = document.getElementById('name').value;
+			var email = document.getElementById('email').value;
+			var gender = document.getElementById('gender').value;
+			var pass = document.getElementById('pass').value;
+			var confirm = document.getElementById('confirm').value;
+			var uType = document.getElementById('uType').value;
+			var check = document.getElementById('check').value;
+
+			if (name == "")
+			{
+				document.getElementById('name').innerHTML = "JS works"
+				error_is = 1;
+			}
+			}
+
+
+		}
 		
+		</script>
+        </body>
+        </html>
